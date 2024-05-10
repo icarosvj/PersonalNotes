72c14939255fd0bea4aa5ecc3ff2e609e98e85fb
Icaro Abreu Johann <icaroabreujohann07@gmail.com>
2024-05-10 10:03:57 -0300

    Add - Show Task

--- app.js
+++ app.js
@@ -37,26 +37,48 @@ navDisplayTasks.addEventListener("click", () => {
 
 let taskInput = document.getElementById('taskInput')
 let taskAddBtn = document.getElementById('taskAddBtn')
+let listUL = document.getElementById('listUL')
 
 taskList = []
 
 //
 
 function reloadTaskList() {
-    let taskListMap = taskList.map(function(task)
-    {
+    let taskListMap = taskList.map(function (task) {
         return `
-        <p class="taskItem">${task}</p>
-        
-        <button class="taskNoteBtn" value="">
-            <img src="images/iconNotes.svg">
-        </button>
+        <div class="task">
+            <p class="taskItem">${task}</p>
 
-        <button class="deleteBtn" value="">
-            <img src="images/deleteIcon.svg">
-        </button>
+            <button class="taskNoteBtn">
+                <img src="images/iconNotes.svg">
+            </button>
+
+            <button class="deleteBtn" value="${taskList.indexOf(task)}">
+                <img src="images/deleteIcon.svg">
+            </button>
+        </div>
         `
-    
+
     })
+    taskListMap = taskListMap.join("")
+    listUL.innerHTML = taskListMap
+    console.table(taskList)
 }
 
+function addTaskOnList() {
+    if (taskInput.value === "") {
+        window.alert('insira')
+    } else {
+        taskList.push(taskInput.value)
+        taskInput.value = ''
+        taskInput.focus()
+        console.log(taskList)
+        //Adicionar no HTML
+        reloadTaskList()
+    }
+}
+
+
+taskAddBtn.addEventListener('click', () => {
+    addTaskOnList()
+})

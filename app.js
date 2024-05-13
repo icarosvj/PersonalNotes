bd1eb19be4360db14e5b0279c71d7b45036e10a8
icarosvj <icaroabreujohann07@gmail.com>
2024-05-13 16:59:05 -0300

    Added Delete and Edit Task

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ let taskInput = document.getElementById('taskInput')
 let taskAddBtn = document.getElementById('taskAddBtn')
 let listUL = document.getElementById('listUL')
 
+let indexTaskObj = 0
 let taskList = []
 //
 
@@ -46,32 +47,48 @@ function reloadTaskList() {
     let taskListMap = taskList.map(function (task) {
         return `
         <div class="task">
-            <p class="taskItem">${task}</p>
+            <p class="taskItem">${task.title}</p>
 
-            <button class="taskNoteBtn" value="${taskList.indexOf(task)}" onclick"editTask(this.value)">
+            <button class="taskNoteBtn" value="${taskList.indexOf(task)}" onclick="editTask(this.value)">
                 <img src="images/iconNotes.svg">
             </button>
 
             <button class="deleteBtn" value="${taskList.indexOf(task)}" onclick="deleteTask(this.value)">
                 <img src="images/deleteIcon.svg">
             </button>
+
+            <div class="taskEdit" id="taskEdit${task.id}">
+                <div class="taskEditTop">
+                    <input class="taskEditTitle" value="${task.title}" spellcheck="false" id="taskTitle${taskList.indexOf(task)}">
+                    <button class="taskEditCloseBtn" value="${taskList.indexOf(task)}" onclick="closeTask(this.value)">
+                        <img src="/images/close.svg" alt="" srcset="">
+                    </button>
+            </div>
+                <textarea name="taskEditText" id="taskEditText${task.id}" class="taskEditText" cols="35" rows="10" spellcheck="false" placeholder="Insert your Text">${task.text}</textarea>
+            </div>
         </div>
+
+
         `
 
     })
     taskListMap = taskListMap.join("")
     listUL.innerHTML = taskListMap
-    console.table(taskList)
 }
 
 function addTaskOnList() {
     if (taskInput.value === "") {
         window.alert('insira')
     } else {
-        taskList.push(taskInput.value)
+        taskList.push({ id: indexTaskObj, })
+        taskList[indexTaskObj].title = taskInput.value
+        taskList[indexTaskObj].text = ""
+
+        indexTaskObj++
+
         taskInput.value = ''
         taskInput.focus()
-        localStorage.setItem("taskList", JSON.stringify(taskList));
+        //localStorage.setItem("taskList", JSON.stringify(taskList));
         //Adicionar no HTML
         reloadTaskList()
     }
@@ -80,7 +97,7 @@ function addTaskOnList() {
 
 taskAddBtn.addEventListener('click', () => {
     addTaskOnList()
-    console.log(taskList)
+    console.table(taskList)
 })
 
 
@@ -90,6 +107,20 @@ function deleteTask(value) {
 }
 
 
-function editTask(index) {
+function editTask(value) { 
+    let editMenu = document.querySelector(`div#taskEdit${taskList[value].id}`)
+    editMenu.style.display = "flex"
+}
 
+function closeTask(value) {
+    let editMenu = document.querySelector(`div#taskEdit${taskList[value].id}`)
+    editMenu.style.display = "none"
+
+    //get text
+    let currentTaskTitle = document.getElementById(`taskTitle${value}`).value
+    taskList[value].title = currentTaskTitle
+
+    let currentTaskText = document.getElementById(`taskEditText${value}`).value
+    taskList[value].text = currentTaskText
+    reloadTaskList()
 }
